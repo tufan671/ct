e8bce8b744c148f7bbfb8f008cdb83e0ea3d6c37
tufan671 <65157420+tufan671@users.noreply.github.com>
2022-01-08 03:41:32 -0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,14 @@
+﻿const config = { 
+//------------Connections-----------//
+    Client_Token: '',
+    MongoDB_ConnectURL: 'mongodb+srv://Arcenio:mea3jq14v1@cluster0.qyyew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    //------------BotSettings-----------//
+    Prefix: '!!',
+    BotOwners: ["589079197906108416"],
+    Custom_Status_Text: 'Arcenio ❤️ Valentina',
+    Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
+    Custom_Status: 'online', // => dnd / idle / online / invisible
+    VoiceChannelID: '914513781890113539'
+};
+  
+module.exports = config;
